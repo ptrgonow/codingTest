5abc14b07da45abe76101e554447ee44e7ebfbc5
Patrick <159615827+ptrgonow@users.noreply.github.com>
2024-08-24 20:40:48 +0900

    [level 0] Title: 대문자로 바꾸기, Time: 0.03 ms, Memory: 71.8 MB -BaekjoonHub

---
+++ 프로그래머스/0/181877. 대문자로 바꾸기/대문자로 바꾸기.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+class Solution {
+    public String solution(String myString) {
+        String answer = "";
+        
+        // 입력된 myString 변수 내부의 글자 전부 uppercase로 변경
+        
+        answer = myString.toUpperCase();
+        
+        
+        return answer;
+    }
+}
