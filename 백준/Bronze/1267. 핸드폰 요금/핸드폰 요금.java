2f1f71ee92f1a4e7e7a598909589850eba98bfb3
Patrick <159615827+ptrgonow@users.noreply.github.com>
2024-08-31 02:56:23 +0900

    [Bronze III] Title: 핸드폰 요금, Time: 136 ms, Memory: 16232 KB -BaekjoonHub

---
+++ 백준/Bronze/1267. 핸드폰 요금/핸드폰 요금.java
@@ -0,0 +1,34 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader r = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter w = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int a = Integer.parseInt(r.readLine());
+        String[] s = r.readLine().split(" ");
+        int[] n = Arrays.stream(s).mapToInt(Integer::parseInt).toArray();
+
+        int yre = Arrays.stream(n)
+                .map(t -> (t / 30 + 1) * 10)
+                .sum();
+
+        int mre = Arrays.stream(n)
+                .map(t -> (t / 60 + 1) * 15)
+                .sum();
+
+        if (yre < mre) {
+            w.write("Y " + yre + "\n");
+        } else if (yre > mre) {
+            w.write("M " + mre + "\n");
+        } else {
+            w.write("Y M " + yre + "\n");
+        }
+
+        w.flush();
+        w.close();
+        r.close();
+    }
+}
